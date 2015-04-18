f9f9cb818119b8c609b3ecab26564e1200ed5e05
Drazatlam <lulu.brisard@hotmail.fr>
2015-04-18 14:32:48 +0200

    Rangement des scripts dans plusieurs dossiers

---
+++ Unity/Assets/Scripts/Test/TestLogger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestLogger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Logger.logLvl = Logger.Type.DEBUG;
+		Logger.logFile = "C:/Users/lucas/Desktop/log.txt";
+		Logger.Debug("test debug");
+		Logger.Error("test error");
+		Logger.Trace("test trace");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
