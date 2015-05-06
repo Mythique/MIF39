e7a1116c1300b1ee1e3b6b044771bad8fc6ded66
5c5 <colas.p.s@gmail.com>
2015-05-06 12:14:06 +0200

    modif du serveur pour traitement

--- BASE_C++/Samples/ServerInTheMiddle/main.cpp
+++ BASE_C++/Samples/ServerInTheMiddle/main.cpp
@@ -10,49 +10,6 @@ extern void __attachWavefront(void);
 
 int main(int argc, char** argv) {
 
-    /*
-    __attachQImage();
-    __attachWavefront();
-
-    QUuid fake;
-    SimpleTcpStartPoint::Options options;
-    options.maximumConnectedClients = 200;
-    options.connectionPort = 3000;
-    EncTcpStartPoint tcp(options);
-    ServerManager::getInstance()->linkTcp(tcp);
-    /*ServerManager::getInstance()->startConnection();
-    QUuid client;
-
-    /* Tant qu'on est actif
-     *  Connexion tant que pas de client
-     *      Lire le message
-     *      Fin du client
-     * Fin de la connexion
-    */
-
-    /*std::cout << "Loading file" << std::endl;
-    FileDescriptor file ( argv[1]);
-    std::cout << "Descriptor got" << std::endl;
-    SharedResourceList ress = ResourceHolder::Load(file);
-    std::cout << "Got list" << std::endl;
-    SharedResourcePtr ptr = ress[0];
-    std::cout << "Got pointer" << std::endl;
-    QUuid quid = ptr.data()->getUUID();
-
-    std::cout << "QUuid : " << quid.toString().toStdString() << std::endl;
-    /*
-    while(true) {
-        while(client == fake) {
-            client = ServerManager::getInstance()->getConnection()->listen();
-        }
-        std::cout << "Client found" <<  client.toString().toStdString() << std::endl;
-        ServerManager::getInstance()->interpret(client);
-        std::cout << "Client disconnected" << std::endl;
-        client = fake;
-    }
-
-    ServerManager::getInstance()->stopConnection();*/
-
     __attachQImage();
     __attachWavefront();
 
@@ -71,24 +28,16 @@ int main(int argc, char** argv) {
      *      Fin du client
      * Fin de la connexion
     */
-    FileDescriptor file ( argv[1]);
+    /*FileDescriptor file ( argv[1]);
     SharedResourceList ress = ResourceHolder::Load(file);
     std::cout << "** Resource loaded" << std::endl;
     SharedResourcePtr ptr = ress [0];
     QUuid quid = ptr.data()->getUUID();
 
     std::cout << "QUuid : " << quid.toString().toStdString() << std::endl;
-    /*std::cout << "Loading file" << std::endl;
-        FileDescriptor file ( argv[1]);
-        std::cout << "Descriptor got" << std::endl;
-        SharedResourceList ress = ResourceHolder::Load(file);
-        std::cout << "Got list" << std::endl;
-        SharedResourcePtr ptr = ress[0];
-        std::cout << "Got pointer" << std::endl;
+	*/
 
-*/
-
-    while(true) {
+    /*while(true) {
         while(client == fake) {
             client = ServerManager::getInstance()->getConnection()->listen();
         }
@@ -96,7 +45,52 @@ int main(int argc, char** argv) {
         ServerManager::getInstance()->interpret(client);
         std::cout << "Client disconnected" << std::endl;
         //client = fake;
+    }//*/
+	
+	while ( true ) {
+        // attente d'un client
+        std::cout << "Wait for client" << std::endl;
+        QUuid client;
+        while ( client == fake ) {
+            client = tcp.listen();
+        }
+        std::cout << "Client " << client.toString().toStdString() << " connected" << std::endl;
+        // envoi l'UUID du 1er monde lors de la connexion d'un client
+        QUuid world = ResourceHolder::GetAllByTypeName("World" ) [ 0 ]->getUUID();
+        EncByteBuffer bworld = ::toBuffer (world);
+		bworld.setType(4);
+        if ( tcp.send ( client, bworld ) ) {
+            unsigned long long sended = 0;
+			while(true){
+				if(! ServerManager::getInstance()->interpret(client))
+					break;
+			}
+            
+			
+			/*while ( true ) {
+                // attente d'une requete du client
+                EncByteBuffer request;
+                QUuid uuid;
+                unsigned long long index = 0;
+                if ( ! server.receive(client,request) )
+                    break;
+                index = ::fromBuffer(request,index, uuid);
+                std::cout << "Request for " << uuid.toString().toStdString() << "..."; std::cout.flush();
+                // recuperation de la ressource
+                SharedResourcePtr resource = ResourceHolder::GetByUUID(uuid);
+                // conversion binaire
+                ByteBuffer reply = ResourceHolder::ToBuffer(resource);
+                // envoi de la reponse
+                if ( ! server.send(client,reply) )
+                    break;
+                sended += reply.getLength();
+                std::cout << " replied" << std::endl;
+            }*/
+            std::cout << "Sent " << sended << " bytes" << std::endl;
+        }
+        // deconnexion du client
     }
+
 
     ServerManager::getInstance()->stopConnection();
     return 0;
