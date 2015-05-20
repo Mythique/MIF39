1a441939f80e3ceff6bb892eeab570a1cbb995f2
5c5 <colas.p.s@gmail.com>
2015-05-20 16:32:21 +0200

    Revert "gestion multi client du serveur"
    This reverts commit 63808a843c368aaa60eff2268752166cc27df376.

--- BASE_C++/Samples/ServerInTheMiddle/main.cpp
+++ BASE_C++/Samples/ServerInTheMiddle/main.cpp
@@ -22,7 +22,6 @@ int main(int argc, char** argv) {
     ServerManager::getInstance()->linkTcp(tcp);
     ServerManager::getInstance()->startConnection();
     QUuid client;
-	int pid;
 
     /* Tant qu'on est actif
      *  Connexion tant que pas de client
@@ -33,8 +32,11 @@ int main(int argc, char** argv) {
     FileDescriptor file ( argv[1]);
     ResourceHolder::Import(argv[1]);
     std::cout << "** Resource loaded : " << ResourceHolder::AllKeys ().size () << std::endl;
-    
-	std::cout << "Before getting quid" << std::endl;
+    /*SharedResourcePtr ptr = ress [0];
+    std::cout << "Got ptr" << std::endl;
+    QUuid quid = ptr.data()->getUUID();
+    std::cout << "quid got" << std::endl;*/
+    std::cout << "Before getting quid" << std::endl;
     SharedResourceList list = ResourceHolder::GetAllByTypeName("Mesh");
     std::cout << "list got " << list.size() << std::endl;
     QUuid mesh = list[0] ->getUUID();
@@ -47,26 +49,58 @@ int main(int argc, char** argv) {
         while(client == fake) {
             client = ServerManager::getInstance()->getConnection()->listen();
         }
-        
-		pid = -1;
-		pid = fork();
-		if(pid == 0){
-
-			while(true){
-
-				if(!ServerManager::getInstance()->interpret(client)){
-            		
-					std::cout << "Déconnexion du client" << std::endl;
-					exit(0);
-				}
-			}
+        //std::cout << "Client found" <<  client.toString().toStdString() << std::endl;
+        if(!ServerManager::getInstance()->interpret(client)){
+            client = fake;
+            std::cout << "Effacement du client" << std::endl;
         }
-		else if(pid == -1){
-			std::cout << "Alerte : erreur lors du Fork. Cient non traité" << std::endl;
-		}
-		else
-			client == fake;
-    }
+        //ServerManager::getInstance()->getConnection()->
+    }//*/
+	
+	/*while ( true ) {
+        // attente d'un client
+        std::cout << "Wait for client" << std::endl;
+        QUuid client;
+        while ( client == fake ) {
+            client = tcp.listen();
+        }
+        std::cout << "Client " << client.toString().toStdString() << " connected" << std::endl;
+        // envoi l'UUID du 1er monde lors de la connexion d'un client
+        QUuid world = ResourceHolder::GetAllByTypeName("World" ) [ 0 ]->getUUID();
+        EncByteBuffer bworld = ::toBuffer (world);
+		bworld.setType(4);
+        if ( tcp.send ( client, bworld ) ) {
+            unsigned long long sended = 0;
+			while(true){
+				if(! ServerManager::getInstance()->interpret(client))
+					break;
+			}
+            
+			
+			/*while ( true ) {
+                // attente d'une requete du client
+                EncByteBuffer request;
+                QUuid uuid;
+                unsigned long long index = 0;
+                if ( ! server.receive(client,request) )
+                    break;
+                index = ::fromBuffer(request,index, uuid);
+                std::cout << "Request for " << uuid.toString().toStdString() << "..."; std::cout.flush();
+                // recuperation de la ressource
+                SharedResourcePtr resource = ResourceHolder::GetByUUID(uuid);
+                // conversion binaire
+                ByteBuffer reply = ResourceHolder::ToBuffer(resource);
+                // envoi de la reponse
+                if ( ! server.send(client,reply) )
+                    break;
+                sended += reply.getLength();
+                std::cout << " replied" << std::endl;
+            }*/
+            /*std::cout << "Sent " << sended << " bytes" << std::endl;
+        }
+        // deconnexion du client
+    }*/
+
 
     ServerManager::getInstance()->stopConnection();
     return 0;
